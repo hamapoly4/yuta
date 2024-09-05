455cee307a8c6f67cf8012197eda9071789620b2
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-05 09:33:04 +0100

    Add files via upload

---
+++ double_loop_NEO_0904/SpikeCon/StraRun.cpp
@@ -0,0 +1,20 @@
+﻿#include "StraRun.h"
+#include <stdio.h>
+
+using namespace ev3api;
+
+StraRun::StraRun(Motor& leftmotor, Motor& rightmotor, PDcon* pdcongyro)
+	: mLeftMotor(leftmotor), mRightMotor(rightmotor), mPDconGyro(pdcongyro), Run(PWM)
+{
+	;
+}
+
+void StraRun::run()
+{
+	mturn = mPDconGyro->getTurn();
+	mLeftMotor.setPWM(mfix_pwm - mturn);
+	mRightMotor.setPWM(mfix_pwm + mturn);
+	
+	//printf("Straight Run!!\n");
+	//printf("pwm is %d\n", mfix_pwm);
+}
