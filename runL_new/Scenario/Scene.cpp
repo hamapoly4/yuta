d4ab002a1eec24dc23e6001524a70316dac77dbd
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-12 13:53:40 +0900

    Add files via upload

---
+++ runL_new/Scenario/Scene.cpp
@@ -0,0 +1,20 @@
+﻿#include "Scene.h"
+#include <stdio.h>
+
+unsigned char Scene::mscene_no = 0;
+
+Scene::Scene(Run* run, JudgeMent* judgement)
+	: mRun(run), mJudgeMent(judgement)
+{
+	mcurrent_scene_no = mscene_no++;
+}
+
+void Scene::run()
+{
+	mRun->run();
+}
+
+bool Scene::fin_judge()
+{
+	return mJudgeMent->judge();
+}
