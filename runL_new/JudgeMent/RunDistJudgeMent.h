3dc18c4e9a649664fd3127ae591320de619bbc1f
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-12 13:49:43 +0900

    Add files via upload

---
+++ runL_new/JudgeMent/RunDistJudgeMent.h
@@ -0,0 +1,19 @@
+﻿#ifndef ___CLASS_RUNDISTJUDGEMENT
+#define ___CLASS_RUNDISTJUDGEMENT
+
+#include "JudgeMent.h"
+#include "RunDistDetect.h"
+
+class RunDistJudgeMent : public JudgeMent {
+private:
+	RunDistDetect *mRunDistDetect;
+
+	float mtarget_dist;
+	float mcurrent_dist;
+	
+public:
+	RunDistJudgeMent(float target_dist, RunDistDetect* rundistdetect);
+	bool judge();
+};
+
+#endif // ___CLASS_RUNDISTJUDGEMENT
