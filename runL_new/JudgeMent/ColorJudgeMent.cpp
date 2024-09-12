80b8e80f2152266a30063355131708eb20f08d11
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-12 14:05:18 +0900

    Add files via upload

---
+++ runL_new/JudgeMent/ColorJudgeMent.cpp
@@ -0,0 +1,20 @@
+﻿#include "ColorJudgeMent.h"
+#include <stdio.h>
+
+ColorJudgeMent::ColorJudgeMent(unsigned char target_color, ColorDetect* colordetect)
+	: mtarget_color(target_color), mColorDetect(colordetect)
+{
+	;
+}
+
+bool ColorJudgeMent::judge()
+{
+	mcurrent_color = mColorDetect->getColor();
+
+	if (mcurrent_color == mtarget_color)
+	{
+		return true;
+	}
+
+	return false;
+}
---
+++ runL_new/SpikeCon/abstract_run.h
@@ -0,0 +1,17 @@
+﻿/*--------------------------------------------
+ *	FILE NAME : abstract_run
+ *	FILE TYPE : Header File
+ *	AUTHOR    : Yuta Kondo
+ * 	SUMMARY   : Abstract Class &
+ *		    Sub Class
+ *--------------------------------------------*/
+
+#ifndef ___ABSTRACT_CLASS
+#define ___ABSTRACT_CLASS	// インクルードカード
+
+class Run {
+public:
+	virtual void run() = 0;	// 純粋仮想関数
+};
+
+#endif // ___ABSTRACT_CLASS
