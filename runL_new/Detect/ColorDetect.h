d145a372a2ba59167b1bb472d39df8a509fe84ce
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-12 13:44:19 +0900

    Add files via upload

---
+++ runL_new/Detect/ColorDetect.h
@@ -0,0 +1,34 @@
+#ifndef ___CLASS_COLORDETECT
+#define ___CLASS_COLORDETECT
+
+#include "ColorSensor.h"
+
+class ColorDetect {
+private:
+    const ev3api::ColorSensor& mColorSensor;
+
+    enum eColor { BLACK, BLUE, ELSE, };
+
+    rgb_raw_t mrgb;
+    float mmax;
+    float mmin;
+    int mhue;
+    int msaturation;
+    int mvalue;
+    unsigned char mcolor;
+
+public:
+    ColorDetect(const ev3api::ColorSensor& colorsensor);
+    unsigned char getColor();
+    void resetColor();
+    int getV();
+    void convHSV();
+    void setRGB();
+    void calcMax();
+    void calcMin();
+    void calcH();
+    void calcS();
+    void calcV();
+};
+
+#endif // ___CLASS_COLORDETECT
