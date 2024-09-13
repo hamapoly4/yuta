a21af8a3fdafbb4f34bad21f90db56ed31ef6700
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-13 18:31:01 +0900

    Add files via upload

---
+++ runL_new/Scenario/Scenario.cpp
@@ -0,0 +1,58 @@
+﻿#include "Scenario.h"
+#include <stdio.h>
+
+using namespace ev3api;
+
+Scenario::Scenario(Scene* scene[], Motor& leftmotor, Motor& rightmotor,
+					 	Motor& leftmotorencoder, Motor& rightmotorencoder,
+							Motor& resetleft, Motor& resetright, ColorDetect* colordetect)
+	: mscene_no(TOP), mscene_switch_flag(false), mscenario_fin_flag(false),
+	  mLeftMotor(leftmotor), mRightMotor(rightmotor),
+	  mLeftMotorEncoder(leftmotorencoder), mRightMotorEncoder(rightmotorencoder),
+	  mResetLeft(resetleft), mResetRight(resetright), mColorDetect(colordetect)
+{
+	unsigned char i;
+	for (i = 0; i < SIZE; i++)
+	{
+		mScene[i] = scene[i];
+	}
+}
+
+void Scenario::run()
+{
+	if (mscene_switch_flag == true)
+	{
+		switch_scene();
+		return;
+	}
+	
+	mColorDetect->convHSV();
+	
+	mScene[mscene_no]->run();
+	mscene_switch_flag = mScene[mscene_no]->fin_judge();
+}
+
+void Scenario::switch_scene()
+{
+	reset_device();
+	mscene_no++;
+	mscene_switch_flag = false;	
+
+	if (mscene_no > LAST)
+	{
+		mscenario_fin_flag = true;
+	}
+}
+
+void Scenario::reset_device()
+{
+	mResetLeft.reset();
+	mResetRight.reset();
+	
+	printf("reset device!!\n\n");
+}
+
+bool Scenario::finish()
+{
+	return mscenario_fin_flag;
+}
---
+++ runL_new/Scenario/Scenario.h
@@ -0,0 +1,38 @@
+﻿#ifndef ___CLASS_SCENARIO
+#define ___CLASS_SCENARIO
+
+#define SIZE 7
+
+#include "Scene.h"
+#include "Motor.h"
+#include "ColorDetect.h"
+
+class Scenario {
+private:
+	static const unsigned char TOP = 0;
+	static const unsigned char LAST = 6;
+
+	Scene* mScene[SIZE];
+	ev3api::Motor&		 mLeftMotor;
+	ev3api::Motor&		 mRightMotor;
+	ev3api::Motor&       mLeftMotorEncoder;
+	ev3api::Motor&       mRightMotorEncoder;
+	ev3api::Motor&       mResetLeft;
+	ev3api::Motor&       mResetRight;
+	ColorDetect*		 mColorDetect;
+
+	unsigned char mscene_no;
+	bool mscene_switch_flag;
+	bool mscenario_fin_flag;
+
+public:
+	Scenario(Scene* scene[], ev3api::Motor& leftmotor, ev3api::Motor& rightmotor,
+				ev3api::Motor& leftmotorencoder, ev3api::Motor& rightmotorencoder,
+					ev3api::Motor& resetleft, ev3api::Motor& resetright, ColorDetect* colordetect);
+	void run();
+	void switch_scene();
+	void reset_device();
+	bool finish();
+};
+
+#endif // ___CLASS_SCENARIO
