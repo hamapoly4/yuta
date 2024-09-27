6fa7e7c45a085f5c93227e4ebe7cd362e55fdb8a
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-27 17:20:44 +0900

    Add files via upload

---
+++ double_loop_NEO_new01/Run/Run.h
@@ -0,0 +1,30 @@
+﻿/*--------------------------------------------
+ *	FILE NAME : abstract_run
+ *	FILE TYPE : Header File
+ *	AUTHOR    : Yuta Kondo
+ * 	SUMMARY   : Abstract Class &
+ *		    Sub Class
+ *--------------------------------------------*/
+
+#ifndef ___CLASS_RUN
+#define ___CLASS_RUN	// インクルードカード
+
+#include "Motor.h"
+
+class Run {
+public:
+	enum eSpeed { LOW, MID, HIGH };
+
+	virtual void run() = 0;	// 純粋仮想関数
+
+protected:
+	ev3api::Motor& mLeftMotor;
+	ev3api::Motor& mRightMotor;
+
+	eSpeed speed;
+	int8_t turn;
+	int8_t pwm;
+	Run(ev3api::Motor& leftmotor, ev3api::Motor& rightmotor, eSpeed s);
+};
+
+#endif // ___CLASS_RUN
