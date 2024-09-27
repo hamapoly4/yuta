04a74be6083de6ad2d4a7883d2adf230d03f60c6
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-09-27 16:01:54 +0900

    Add files via upload

---
+++ double_loop_NEO_new01/Run/RotateRun.h
@@ -0,0 +1,22 @@
+﻿#ifndef ___CLASS_ROTATERUN
+#define ___CLASS_ROTATERUN	// インクルードカード
+
+#include "Run.h"
+#include "GyroStopP.h"
+#include "Motor.h"
+
+class RotateRun : public Run {
+public:
+	enum eRota { LEFT, RIGHT, };
+
+	RotateRun(eRota r, GyroStopP* gyrostopp, ev3api::Motor& leftmotor, ev3api::Motor& rightmotor, eSpeed s = LOW);
+
+	void run();
+
+private:
+	GyroStopP* mGyroStopP;
+
+	eRota rota;
+};
+
+#endif // ___CLASS_ROTATERUN
---
+++ double_loop_NEO_new01/Run/StraRun.h
@@ -0,0 +1,17 @@
+﻿#ifndef ___CLASS_STRARUN
+#define ___CLASS_STRARUN	// インクルードカード
+
+#include "Run.h"
+#include "GyroStraPD.h"
+#include "Motor.h"
+
+class StraRun : public Run {
+public:
+	StraRun(GyroStraPD* gyrostrapd, ev3api::Motor& leftmotor, ev3api::Motor& rightmotor, eSpeed s);
+	void run();
+
+private:
+	GyroStraPD* mGyroStraPD;
+};
+
+#endif // ___CLASS_STRARUN
