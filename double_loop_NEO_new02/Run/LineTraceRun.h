f12cfc3fdad42af638a572d7527c3aa8ee287bc4
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-10-01 06:48:48 +0900

    Add files via upload

---
+++ double_loop_NEO_new02/Run/LineTraceRun.h
@@ -0,0 +1,20 @@
+﻿#ifndef ___CLASS_LINETRACERUN
+#define ___CLASS_LINETRACERUN	// インクルードカード
+
+#include "Run.h"
+#include "Control.h"
+#include "Motor.h"
+
+class LineTraceRun : public Run {
+public:
+	enum eEdge { LEFT, RIGHT, };
+
+	LineTraceRun(eEdge e, Control* linetracepd, ev3api::Motor& leftmotor, ev3api::Motor& rightmotor, eSpeed s);
+	void run();
+
+private:
+	Control* mLineTracePD;
+	eEdge edge;
+};
+
+#endif // ___CLASS_LINETRACERUN
