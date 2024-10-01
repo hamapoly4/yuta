acae90307ece3f838e36234710ce90b1ecd39cd7
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-10-01 18:04:58 +0900

    Add files via upload

---
+++ double_loop_NEO_new03/Control/GyroStopP.h
@@ -0,0 +1,25 @@
+#ifndef ___CLASS_GYROSTOPP
+#define ___CLASS_GYROSTOPP
+
+#include "Control.h"
+#include "AngleMonitor.h"
+
+class GyroStopP : public Control {
+public:
+    GyroStopP(AngleMonitor* anglemonitor);
+    int getTurn();
+    void calcTurn();
+    void calcP();
+
+private:
+    AngleMonitor* mAngleMonitor;
+
+    static constexpr float PGAIN = 0.17;
+    static const int threathold[4];
+
+	static int8_t size;
+
+	int8_t cnt;
+};
+
+#endif // ___CLASS_GYROSTOPP
