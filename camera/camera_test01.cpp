b7a6d08bdd59743060ecac5bf7ee97475af87c11
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-11-21 11:30:35 +0900

    Add files via upload

--- camera/camera_test01.cpp
+++ camera/camera_test01.cpp
@@ -1,37 +1,33 @@
 #include <opencv2/opencv.hpp>
-#include <iostream>
 
-int main()
-{
-	// V4L2
-	cv::VideoCapture cap;
-	cap.open("/dev/video0", cv::CAP_V4L2);
+using namespace cv;
+using namespace std;
 
-	if (!cap.isOpened())
-	{
-		std::cerr << "Error : don't open camera." << std::endl;
-		return -1;
-	}
+int main() {
+    // カメラを起動（カメラ番号0を指定）
+    VideoCapture capture(0);
+    if (!capture.isOpened()) {
+        cout << "カメラが開けませんでした。" << endl;
+        return -1;
+    }
 
-	cv::Mat frame;
-	while (true)
-	{
-		cap >> frame;
-		if (frame.empty())
-		{
-			printf("don't capture frame!!\n");
-			break;
-		}
+    Mat frame;
+    while (true) {
+        // カメラから画像を取得
+        capture >> frame;
+        if (frame.empty()) {
+            cout << "フレームが取得できませんでした。" << endl;
+            break;
+        }
 
-		cv::imshow("Camera", frame);
+        // 画像を表示
+        imshow("カメラ画像", frame);
 
-		if (cv::waitKey(1) == 'q')
-		{
-			break;
-		}
-	}
+        // ESCキーが押されたらループを終了
+        if (waitKey(1) == 27) {
+            break;
+        }
+    }
 
-	cap.release();
-	cv::destroyAllWindows();
-	return 0;
+    return 0;
 }
